2cc8e9ee950810884f84e0bfd449affe36091cbc
Mike Gaughan <michael.t.gaughan@gmail.com>
2016-02-28 23:37:18 -0700

    Select list

--- src/app/select-list/select-list.component.js
+++ src/app/select-list/select-list.component.js
@@ -1,18 +1,30 @@
 (function(){
 
-    var selectList = {
-        bindings: {
-            items: '<',
-            activeItem: '='
-        },
-        templateUrl: 'app/select-list/select-list.view.html',
-        controller: 'SelectListController as vm'
+    angular
+        .module('tc.selectList')
+        .component('selectList', selectList())
+        .controller('SelectListController', SelectListController);
+
+    function selectList() {
+        return {
+            bindings: {
+                items: '<',
+                activeItem: '='
+            },
+            templateUrl: 'app/select-list/select-list.view.html',
+            controller: 'SelectListController as vm'
+        }
     };
 
     function SelectListController() {
         var vm = this;
 
+        vm.isActive = isActive;
         vm.updateActiveItem = updateActiveItem;
+
+        function isActive(item){
+            return item == vm.activeItem;
+        };
 
         function updateActiveItem(value){
             vm.activeItem = value;
@@ -21,9 +33,4 @@
         return this;
     };
 
-    angular
-        .module('tc.selectList')
-        .component('selectList', selectList)
-        .controller('SelectListController', SelectListController)
-
-}());
+})();
